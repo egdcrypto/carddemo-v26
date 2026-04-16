a068a2f355bddc866cf106eb79df77dcf3db9543
Eric Diana <taylormade7978@hotmail.com>
2026-04-16 12:21:48 -0500

    feat(feature/s-27): src/domain/userprofile/command/update_profile_details.go

---
+++ src/domain/userprofile/command/update_profile_details.go
@@ -0,0 +1,8 @@
+package command
+
+// UpdateProfileDetailsCmd is a command to update user profile info.
+type UpdateProfileDetailsCmd struct {
+	FirstName string
+	LastName  string
+	Email     string
+}
